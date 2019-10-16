0eaf58f258f6f19356907cdb00b5698482abb978
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-10-16 18:53:21 +0200

    Chat UI tweaks + socket.io

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -1,4 +1,16 @@
 import React from "react";
+import io from "socket.io-client";
+
+const devSocketUrl = "http://localhost:3001";
+const prodSocketUrl = "https://chat.jsramverk.evilbengt.me:3001";
+let socketUrl;
+
+if (window.location.origin.includes("localhost")) {
+    socketUrl = devSocketUrl;
+} else {
+    socketUrl = prodSocketUrl;
+}
+
 
 class Chat extends React.Component {
     constructor(props) {
@@ -7,6 +19,8 @@ class Chat extends React.Component {
             with: (update) => {
                 return Object.assign(this.state, update);
             },
+            connected: false,
+            active: false,
             nick: "",
             newMessage: "",
             messages: []
@@ -14,19 +28,41 @@ class Chat extends React.Component {
     }
 
     componentDidMount() {
-        
+        this.socket = io(socketUrl);
+        const that = this;
+
+        that.socket.on("connect", function () {
+            that.setState(that.state.with({
+                connected: true
+            }));
+
+            that.socket.on("messageFromServer", function (message) {
+                that.addMessage(message);
+            });
+
+            that.socket.on("newClient", function (message) {
+                that.addMessage(message + " anslöt till konversationen.");
+            })
+        });
     }
 
     render() {
+        return this.state.connected && this.state.active ? this.Main() : this.Welcome();
+    }
+
+    Welcome() {
         return (
             <main className="main-content">
-                <article className="chat">
-                    <h1 className="chat-title">Chatt</h1>
-                    <form className="form chat-form"
+                <article>
+                    <h1>Chatt</h1>
+                    <form className="form"
                         onSubmit={e => {
                             e.preventDefault();
-                            this.submit()}
-                        }
+                            this.setState(this.state.with({
+                                active: true
+                            }));
+                            this.socket.emit("clientConnected", this.state.nick);
+                        }}
                     >
                         <label className="input-label chat-input-name">
                             Namn<br/>
@@ -37,6 +73,26 @@ class Chat extends React.Component {
                                 })) }
                             />
                         </label>
+                        <button type="submit" className="button button-submit">
+                            Börja chatta
+                        </button>
+                    </form>
+                </article>
+            </main>
+        )
+    }
+
+    Main() {
+        return (
+            <main className="main-content">
+                <article className="chat">
+                    <h1 className="chat-title">Chatt</h1>
+                    <form className="form chat-form"
+                        onSubmit={e => {
+                            e.preventDefault();
+                            this.submit()}
+                        }
+                    >
                         <div className="chat-input-footer">
                             <label className="input-label chat-input-message">
                                 Meddelande<br/>
@@ -53,13 +109,24 @@ class Chat extends React.Component {
                         </div>
                     </form>
                     <ul className="chat-messages">
-                        { this.state.messages.map(message => (
-                            <ChatMessage
-                                nick={ message.nick }
-                                time={ message.time }
-                                body={ message.body }
-                            />
-                        )) }
+                        { this.state.messages.map((message, index) => {
+                            if (typeof(message) === "object") {
+                                return (
+                                    <ChatMessage key={ index }
+                                        nick={ message.nick }
+                                        time={ message.time }
+                                        body={ message.body }
+                                        isMe={ message.nick === this.state.nick }
+                                    />
+                                );
+                            } else {
+                                return (
+                                    <Notice key={ index }
+                                        notice={ message }
+                                    />
+                                );
+                            }
+                        }) }
                     </ul>
                 </article>
             </main>
@@ -67,16 +134,31 @@ class Chat extends React.Component {
     }
 
     submit() {
-        console.log("Sending message")
+        const time = (new Date()).toLocaleString();
+
+        this.socket.emit("messageFromClient", {
+            nick: this.state.nick,
+            time: time,
+            body: this.state.newMessage
+        });
         this.setState(this.state.with({
             newMessage: ""
+        }));
+    }
+
+    addMessage(message) {
+        let newMessages = this.state.messages;
+        newMessages.push(message);
+
+        this.setState(this.state.with({
+            messages: newMessages
         }));
     }
 }
 
 function ChatMessage(props) {
     return (
-        <li className="chat-message">
+        <li className={ "chat-message" + (props.isMe ? " is-me" : "") }>
             <span className="chat-message-head">
                 { props.nick }
                 <span className="chat-message-time">
@@ -86,6 +168,14 @@ function ChatMessage(props) {
             <p className="chat-message-body">
                 { props.body }
             </p>
+        </li>
+    );
+}
+
+function Notice(props) {
+    return (
+        <li className="chat-message chat-notice">
+            { props.notice }
         </li>
     );
 }
